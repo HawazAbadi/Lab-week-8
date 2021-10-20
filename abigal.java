f9da9948df423c4b5282aea47e69df3f003a8c2c
HawazAbadi <hawamg7@gmail.com>
2021-10-19 18:15:16 -0700

    update

---
+++ abigal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class abigal {
+
+   public static void main(String[] args) {
+        
+    Scanner sc=new Scanner (System.in);
+        
+    String name;
+    int age;
+    String medical;
+
+    
+
+    System.out.println("Enter your name");
+    name=sc.next();
+    System.out.println("Enter your age");
+    age=sc.nextInt();
+    System.out.println("Enter vaccination name ");
+    medical=sc.next();
+
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("your booster vaccination is " + medical);
+        
+    }
+    
+}
